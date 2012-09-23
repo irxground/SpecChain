9499448a2620a88d22cb4f48a184b27609f74e7c
irxground <irxnjhtchlnrw@gmail.com>
2012-09-24 00:30:00 +0900

    Add project

---
+++ SpecChain.Sample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpecChain.Sample {
+    class Program {
+        static void Main(string[] args) {
+            Spec.RunAllInAssembly(typeof (Program));
+        }
+
+        [Spec]
+        public void Test() {
+            Console.WriteLine("Hello, world");
+        }
+    }
+}
---
+++ SpecChain/Spec.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace SpecChain {
+    public class Spec {
+
+
+        public static void RunAllInAssembly<T>() {
+            RunAllInAssembly(typeof (T));
+        }
+
+        public static void RunAllInAssembly(Type type) {
+            RunAllInAssembly(type.Assembly);
+        }
+
+        public static void RunAllInAssembly(Assembly assembly) {
+            var args = new object[] {};
+            foreach (var type in assembly.GetTypes()) {
+                object obj = null;
+                foreach (var m in type.GetMethods()) {
+                    if (m.GetCustomAttributes(typeof(SpecAttribute), false).Length > 0) {
+                        if (m.IsStatic) {
+                            m.Invoke(null, args);
+                        } else {
+                            obj = obj ?? Activator.CreateInstance(type);
+                            m.Invoke(obj, args);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
